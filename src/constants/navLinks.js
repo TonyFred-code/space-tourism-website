d755233ff96a650aefc061604e219bc5c3717cc0
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-02-21 08:43:08 +0100

    feat: create constants file for navigation links

---
+++ src/constants/navLinks.js
@@ -0,0 +1,24 @@
+const NAV_LINKS = [
+  {
+    name: "home",
+    index: "00",
+    path: "/",
+  },
+  {
+    name: "destination",
+    index: "01",
+    path: "/destination",
+  },
+  {
+    name: "crew",
+    index: "02",
+    path: "/crew",
+  },
+  {
+    name: "technology",
+    index: "03",
+    path: "/technology",
+  },
+];
+
+export { NAV_LINKS };
