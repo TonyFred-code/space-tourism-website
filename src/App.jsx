e8d0abd92094451f0ac8bd5decdb99a2d009803f
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-02-21 15:58:42 +0100

    refactor: move page layout to its page
    move header and home page background to Home page component
    remove layout content from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,3 @@
-import { Outlet } from "react-router-dom";
-import Header from "./components/Header";
-
 export default function App() {
-  return (
-    <div className="cover flex min-h-screen flex-col bg-blue-900 bg-[url('/assets/home/background-home-mobile.jpg')] bg-no-repeat md:bg-[url('/assets/home/background-home-tablet.jpg')] bg-size-[100%_100%] lg:bg-[url('/assets/home/background-home-desktop.jpg')]">
-      <Header />
-      <Outlet />
-    </div>
-  );
+  return <div></div>;
 }
