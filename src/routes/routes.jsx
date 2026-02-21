3ba48823b2af9ecd073d12bf87d568de9916fc72
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-02-21 15:59:47 +0100

    refactor: move Home component from child to root
    remove App component from root and set Home component as page index comp

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,17 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App.jsx";
 import Home from "../pages/Home.jsx";
+import Error from "../pages/Error.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-    ],
+    element: <Home />,
+    errorElement: <Error />,
   },
 ]);
 
