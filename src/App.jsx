595ee7d01a912a1a6810a283aef4b47dd80d85c9
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-02-21 08:51:37 +0100

    feat: add header outlet and mobile navbar comps
    turn App component to project wide page layout wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import MobileNavBar from "./components/MobileNavBar";
+import { useState } from "react";
+
 export default function App() {
-  return <div></div>;
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <div className="cover flex min-h-screen flex-col bg-blue-900 bg-[url('/assets/home/background-home-mobile.jpg')] bg-no-repeat md:bg-[url('/assets/home/background-home-tablet.jpg')] bg-size-[100%_100%] lg:bg-[url('/assets/home/background-home-desktop.jpg')]">
+      <Header
+        mobileMenuOpen={mobileMenuOpen}
+        setMobileMenuOpen={setMobileMenuOpen}
+      />
+      <Outlet />
+      <MobileNavBar open={mobileMenuOpen} setOpen={setMobileMenuOpen} />
+    </div>
+  );
 }
