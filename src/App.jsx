b687ef5198cc308dc27d752165f2732032118ffa
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-02-25 07:45:02 +0100

    feat: add LoadingScreen and dataFetching to App
    fetch data in background while LoadingScreen is shown on App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,56 @@
+import { useEffect, useState } from "react";
+import fetchData from "./helpers/fetchData.js";
+import { AnimatePresence, motion } from "framer-motion";
+import LoadingScreen from "./components/LoadingScreen.jsx";
+import DataContext from "./context/DataContext.jsx";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/routes.jsx";
+
 export default function App() {
-  return <div></div>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const LOADER_DURATION = { min: 900, max: 999 };
+    fetchData()
+      .then((d) => {
+        setData(d);
+      })
+      .finally(() => {
+        setTimeout(
+          () => {
+            setIsLoading(false);
+          },
+          Math.floor(
+            Math.random() * (LOADER_DURATION.max - LOADER_DURATION.min + 1) +
+              LOADER_DURATION.min
+          )
+        );
+      });
+  }, []);
+
+  return (
+    <AnimatePresence mode="wait">
+      {isLoading ? (
+        <motion.div
+          key={"loader"}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <LoadingScreen />
+        </motion.div>
+      ) : (
+        <motion.div
+          key={"app"}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <DataContext.Provider value={data}>
+            <RouterProvider router={router} />
+          </DataContext.Provider>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
 }
