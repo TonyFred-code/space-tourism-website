762441e78aa27092732c30462f25aa175dd73067
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-02-27 05:30:47 +0100

    feat: create Crew page starter page content

---
+++ src/pages/Crew.jsx
@@ -0,0 +1,9 @@
+import Header from "../components/Header.jsx";
+
+export default function Crew() {
+  return (
+    <div className="bg-blue-900 bg-[url('/assets/crew/background-crew-mobile.jpg')] md:bg-[url('/assets/crew/background-crew-tablet.jpg')] lg:bg-[url('/assets/crew/background-crew-desktop.jpg')] bg-size-[100%_100%] min-h-screen flex flex-col">
+      <Header />
+    </div>
+  );
+}
