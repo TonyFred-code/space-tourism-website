c7490cb87996fd8b8f94f6f88f86fcfb439402a0
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-03-08 14:48:54 +0100

    style: use inline style to set bg image src
    use a hook and useEffect to set backgroundImage responsively
    remove className backgroundImage string interpolation values
    add missing no-repeat bg property

--- src/components/helpers/PageWrapper.jsx
+++ src/components/helpers/PageWrapper.jsx
@@ -1,5 +1,6 @@
 import { node, string } from "prop-types";
 import Header from "../Header.jsx";
+import { useEffect, useState } from "react";
 
 export default function PageWrapper({
   mobileBgImageSrc,
@@ -7,9 +8,30 @@ export default function PageWrapper({
   desktopBgImageSrc,
   children,
 }) {
+  const [bgSrc, setBgSrc] = useState(mobileBgImageSrc);
+
+  useEffect(() => {
+    function updateBgImage() {
+      if (window.innerWidth >= 1024) {
+        setBgSrc(desktopBgImageSrc);
+      } else if (window.innerWidth >= 768) {
+        setBgSrc(tabletBgImageSrc);
+      } else {
+        setBgSrc(mobileBgImageSrc);
+      }
+    }
+
+    updateBgImage();
+
+    window.addEventListener("resize", updateBgImage);
+
+    return () => window.removeEventListener("resize", updateBgImage);
+  }, [mobileBgImageSrc, tabletBgImageSrc, desktopBgImageSrc]);
+
   return (
     <div
-      className={`min-h-screen flex flex-col bg-blue-900 bg-[url('${mobileBgImageSrc}')] md:bg-[url('${tabletBgImageSrc}')] lg:bg-[url('${desktopBgImageSrc}')] bg-size-[100%_100%]`}
+      className={`min-h-screen flex flex-col bg-blue-900 bg-size-[100%_100%] bg-no-repeat`}
+      style={{ backgroundImage: `url('${bgSrc}')` }}
     >
       <Header />
       {children}
