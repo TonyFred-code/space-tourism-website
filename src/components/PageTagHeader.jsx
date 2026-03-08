a6ab63ba0ff87f24be2df802254c74eb37e96356
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-03-08 14:51:57 +0100

    style: remove unnecessary AnimatePresence wrappers
    remove AnimatePresence wrapper from static animated non-exiting content

--- src/components/PageTagHeader.jsx
+++ src/components/PageTagHeader.jsx
@@ -1,20 +1,18 @@
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { string } from "prop-types";
 
 export default function PageTagHeader({ index, content }) {
   return (
-    <AnimatePresence>
-      <motion.h1
-        className="uppercase flex gap-6 text-white mobile-text-preset-6 justify-center md:justify-start md:tablet-text-preset-5 lg:desktop-text-preset-5"
-        key={index}
-        initial={{ x: -30 }}
-        animate={{ x: 0 }}
-        transition={{ duration: 0.3, ease: "easeIn" }}
-      >
-        <span className="font-bold opacity-25">{index}</span>
-        <span>{content}</span>
-      </motion.h1>
-    </AnimatePresence>
+    <motion.h1
+      className="uppercase flex gap-6 text-white mobile-text-preset-6 justify-center md:justify-start md:tablet-text-preset-5 lg:desktop-text-preset-5"
+      key={index}
+      initial={{ x: -30 }}
+      animate={{ x: 0 }}
+      transition={{ duration: 0.3, ease: "easeIn" }}
+    >
+      <span className="font-bold opacity-25">{index}</span>
+      <span>{content}</span>
+    </motion.h1>
   );
 }
 
